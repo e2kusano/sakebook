1c4afbfb5166be4fab08a44c35c4d831c6426700
e2kusano <kusano@e2info.com>
2019-10-28 22:09:38 +0900

    見た目機能追加 数値だとわかりにくいのでクラスで定数としてもつ

--- docs/src/app/Http/Controllers/StyleController.php
+++ docs/src/app/Http/Controllers/StyleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Create;
+use App\Models\Style;
 use Illuminate\Http\Request;
 
 class StyleController extends Controller
@@ -14,31 +15,31 @@ class StyleController extends Controller
 
     public function cute()
     {
-        $cuties = Create::with('styles')->where('style', 1)->get();
+        $cuties = Create::with('styles')->where('style', Style::CUTE)->get();
         return view('style.cute', compact('cuties'));
     }
 
     public function cool()
     {
-        $cools = Create::with('styles')->where('style', 2)->get();
+        $cools = Create::with('styles')->where('style', Style::COOL)->get();
         return view('style.cool', compact('cools'));
     }
 
     public function simple()
     {
-        $simples = Create::with('styles')->where('style', 3)->get();
+        $simples = Create::with('styles')->where('style', Style::SIMPLE)->get();
         return view('style.simple', compact('simples'));
     }
 
     public function luxury()
     {
-        $luxuries = Create::with('styles')->where('style', 4)->get();
+        $luxuries = Create::with('styles')->where('style', Style::LUXURY)->get();
         return view('style.luxury', compact('luxuries'));
     }
 
     public function eccentric()
     {
-        $eccentrics = Create::with('styles')->where('style', 5)->get();
+        $eccentrics = Create::with('styles')->where('style', Style::ECCENTRIC)->get();
         return view('style.eccentric', compact('eccentrics'));
     }
 
--- docs/src/app/Models/Style.php
+++ docs/src/app/Models/Style.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Style extends Model
 {
-
+    const CUTE = 1;
+    const COOL = 2;
+    const SIMPLE = 3;
+    const LUXURY = 4;
+    const ECCENTRIC = 5;
 }
